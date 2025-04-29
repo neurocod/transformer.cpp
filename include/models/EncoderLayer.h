d5bc85d49814ae58d1e72828c55642dda217e1a1
Krish Modi <kahnscatterkrish@gmail.com>
2025-04-29 18:38:15 -0400

    implemented encoder and decoder layers (untested)

--- include/models/EncoderLayer.h
+++ include/models/EncoderLayer.h
@@ -1,10 +1,11 @@
 #ifndef TRANSFORMER_CPP_ENCODERLAYER_H
 #define TRANSFORMER_CPP_ENCODERLAYER_H
 
-#include "../utils/Tensor.h"
-#include "../layers/MultiHeadAttention.h"
-#include "../layers/PositionwiseFeedForward.h"
-#include "../layers/LayerNorm.h"
+#include "utils/Tensor.h"
+#include "layers/MultiHeadAttention.h"
+#include "layers/PositionwiseFeedForward.h"
+#include "layers/LayerNorm.h"
+#include "layers/Dropout.h"
 #include <memory>
 #include <vector>
 
@@ -12,6 +13,7 @@ class Tensor;
 class MultiHeadAttention;
 class PositionwiseFeedForward;
 class LayerNorm;
+class Dropout;
 
 class EncoderLayer {
 public:
@@ -19,7 +21,7 @@ public:
     EncoderLayer(int embed_dim, int num_heads, int ff_hidden_dim, float dropout_rate = 0.1f);
 
     // Forward pass
-    std::shared_ptr<Tensor> forward(const std::shared_ptr<Tensor>& input);
+    std::shared_ptr<Tensor> forward(std::shared_ptr<Tensor>& input, bool is_training);
 
     // Destructor
     ~EncoderLayer() = default;
@@ -27,8 +29,12 @@ public:
 private:
     MultiHeadAttention self_attention_;
     LayerNorm layernorm1_;
+    Dropout dropout1_;
     PositionwiseFeedForward feed_forward_;
     LayerNorm layernorm2_;
+    Dropout dropout2_;
+
+    float dropout_rate_;
 };
 
 #endif // TRANSFORMER_CPP_ENCODERLAYER_H
