501ac42686965af5282426fb26ca58a69d555334
Krish Modi <kahnscatterkrish@gmail.com>
2025-05-01 16:30:07 -0400

    simple threadpool implementation

---
+++ include/utils/ThreadPool.h
@@ -0,0 +1,37 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <atomic>
+#include <future>
+
+class ThreadPool
+{
+public:
+    explicit ThreadPool(size_t num_threads);
+
+    ~ThreadPool();
+
+    void run_batch(std::vector<std::function<void()>> tasks);
+
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+
+private:
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex queue_mutex_;
+    std::condition_variable condition_;
+    bool stop_;
+    std::mutex completion_mutex_;
+    std::condition_variable completion_condition_;
+    std::atomic<size_t> completed_tasks_ = 0;
+    std::atomic<size_t> expected_tasks_ = 0;
+};
+
+#endif // THREAD_POOL_H
